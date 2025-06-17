6bca930ea37739613e431c22a21beaa626d50731
anatalphonse <123870893+anatalphonse@users.noreply.github.com>
2025-06-17 13:01:09 +0530

    a

--- project/settings.py
+++ project/settings.py
@@ -17,6 +17,7 @@ import dj_database_url
 import dotenv
 dotenv.load_dotenv()
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
